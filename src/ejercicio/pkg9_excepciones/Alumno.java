b69315e34a764fc1b171ce2a6843af506b530cd4
Dani <Dani@DESKTOP-7TVMVOP>
2019-02-08 12:09:09 +0100

    Modificaciones clase alumno y creación de la excepcion MisteriosaExcepcion

--- src/ejercicio/pkg9_excepciones/Alumno.java
+++ src/ejercicio/pkg9_excepciones/Alumno.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * @author Dani
  */
 public class Alumno {
+
     static int num_asignaturas;
     private String nombre;
     ArrayList<Double> notas = new ArrayList<>();
@@ -21,23 +22,23 @@ public class Alumno {
      * Este contructor por defecto solo pone los valores por defecto
      */
     public Alumno() {
-        num_asignaturas=5;
-        nombre="";
-        notas= new ArrayList<>(5);  
+        num_asignaturas = 5;
+        nombre = "";
+        notas = new ArrayList<>(5);
     }
 
     /**
-     * 
+     *
      * @param nombre recibe unicamente el nomnre del alumno
      */
     public Alumno(String nombre) {
         this.nombre = nombre;
-        num_asignaturas=5;
-        notas= new ArrayList<>();
+        num_asignaturas = 5;
+        notas = new ArrayList<>(5);
     }
 
     /**
-     * 
+     *
      * @return devuelve el get y set del nombre del alumno
      */
     public String getNombre() {
@@ -48,18 +49,30 @@ public class Alumno {
         this.nombre = nombre;
     }
 
-    public void pedirNotas() throws RangoException
-    {
-        int nota=1;
-        for (int i = 0; i < this.num_asignaturas; i++) {
+    /**
+     * Este método recorre el numro de notas y las añade al array
+     *
+     * @throws RangoException
+     */
+    public void pedirNotas() throws RangoException {
+        int nota = 1;
+        for (int i = 0; i < 5; i++) {
             Scanner teclado = new Scanner(System.in);
-            
-            System.out.println("Introduce la nota " +nota);
+
+            System.out.println("Introduce la nota " + nota);
             double pedirnota = teclado.nextDouble();
-            notas.add(pedirnota);
+            if (pedirnota >= 0 && pedirnota <= 10) {
+                notas.add(pedirnota);
+            } else {
+                throw new RangoException("Error. Nota no válida");
+            }
+
             nota++;
         }
     }
-    
-    
+
+    public void modificarNota(int posicion, double nnota) throws RangoException, MisteriosaException {
+        
+    }
+
 }
---
+++ src/ejercicio/pkg9_excepciones/MisteriosaException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg9_excepciones;
+
+/**
+ *
+ * @author Dani
+ */
+public class MisteriosaException extends Exception {
+
+    public MisteriosaException(String mensaje) {
+        super(mensaje);
+    }
+}
